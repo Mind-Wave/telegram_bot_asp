2cd9e87590059d47122f2d82fc5d538fd8f8e74e
mindwave <mive.inc@gmail.com>
2019-12-20 13:20:47 +0200

    Add BotClientManager

---
+++ TelegramBotAsp/TelegramBotAsp/Helpers/BotClientManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using Telegram.Bot;
+using TelegramBotAsp.Commands;
+
+namespace TelegramBotAsp.Helpers
+{
+    /// <summary>
+    /// Managing the bot client and bot commands
+    /// </summary>
+    public static class BotClientManager
+    {
+        private static TelegramBotClient _client;
+
+        /// <summary>
+        /// Gets the commands.
+        /// </summary>
+        /// <value>
+        /// The commands.
+        /// </value>
+        public static IReadOnlyCollection<CommandBase> Commands { get; private set; }
+
+        /// <summary>
+        /// Gets the bot client.
+        /// </summary>
+        /// <returns></returns>
+        public static TelegramBotClient GetClient()
+        {
+            if (_client == null)
+            {
+                Initial();
+            }
+
+            return _client;
+        }
+
+        /// <summary>
+        /// Initialization of the bot client
+        /// </summary>
+        public static void Initial()
+        {
+            _client = new TelegramBotClient(BotSettings.TelegramBotKey);
+
+            //Add all new commands here
+            Commands = new List<CommandBase>()
+            {
+
+            };
+
+            string hookLink = $"";
+            _client.SetWebhookAsync(hookLink);
+        }
+    }
+}
