4797f2682badde04dc20180234b6581eeb0799b2
mindwave <mive.inc@gmail.com>
2019-12-20 12:15:31 +0200

    Improve comment style

--- TelegramBotAsp/TelegramBotAsp/Helpers/BotSettings.cs
+++ TelegramBotAsp/TelegramBotAsp/Helpers/BotSettings.cs
@@ -2,15 +2,21 @@
 {
     public static class BotSettings
     {
-        //Link to the bot-based server
-        //Used for webhook
+        /// <summary>
+        ///Link to the bot-based server
+        ///Used for webhook
+        /// </summary>
         public const string BotUrl = "";
 
-        //Telegram bot name
-        //It can be used for calls on behalf of the bot or to check for calls to this bot in the chat
+        /// <summary>
+        ///Telegram bot name
+        ///It can be used for calls on behalf of the bot or to check for calls to this bot in the chat
+        /// </summary>
         public const string BotName = "";
 
-        //A unique key issued by a telegram (BotFather) for your bot
+        /// <summary>
+        /// A unique key issued by a telegram (BotFather) for your bot
+        /// </summary>
         public const string TelegramBotKey = "";
     }
 }
