ccbd49050fb3487b37096ad00c5ee89b285f4fdd
mindwave <mive.inc@gmail.com>
2019-12-20 13:54:16 +0200

    Add MessageController and Routes for it

---
+++ TelegramBotAsp/TelegramBotAsp/Controllers/MessageController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using System.Web.Http.Results;
+using Telegram.Bot.Types;
+using TelegramBotAsp.Commands;
+using TelegramBotAsp.Helpers;
+
+namespace TelegramBotAsp.Controllers
+{
+    public class MessageController : ApiController
+    {
+        /// <summary>
+        /// Telegram message handler
+        /// </summary>
+        /// <param name="update">The update.</param>
+        /// <returns></returns>
+        [Route(Routes.MessageController)]
+        public OkResult Update([FromBody]Update update)
+        {
+            //Taking message text from data sent by telegrams
+            Message msg = update.Message;
+
+            //Check if the message contains one of the bot commands
+            IReadOnlyCollection<CommandBase> commands = BotManager.Commands;
+            foreach (CommandBase command in commands)
+            {
+                if (command.CheckCommandContains(msg.Text))
+                {
+                    //If the message contains a command, then perform the action according to this command
+                    command.Execute(msg, BotManager.GetClient());
+                    break;
+                }
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ TelegramBotAsp/TelegramBotAsp/Helpers/Routes.cs
@@ -0,0 +1,14 @@
+﻿namespace TelegramBotAsp.Helpers
+{
+    /// <summary>
+    /// Web routes
+    /// </summary>
+    public static class Routes
+    {
+        /// <summary>
+        /// The message controller route
+        /// The route by which the telegram will send messages
+        /// </summary>
+        public const string MessageController = @"api/message/update";
+    }
+}
