466615728a84dc7e6ecf04ea075068b7974faa60
mindwave <mive.inc@gmail.com>
2019-12-20 11:37:32 +0200

    Add BotSettings

---
+++ TelegramBotAsp/TelegramBotAsp/Helpers/BotSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace TelegramBotAsp.Helpers
+{
+    public static class BotSettings
+    {
+        //Link to the bot-based server
+        //Used for webhook
+        public const string BotUrl = "";
+
+        //Telegram bot name
+        //It can be used for calls on behalf of the bot or to check for calls to this bot in the chat
+        public const string BotName = "";
+
+        //A unique key issued by a telegram (BotFather) for your bot
+        public const string TelegramBotKey = "";
+    }
+}
