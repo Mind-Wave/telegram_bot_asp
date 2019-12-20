824529c27bff6379aca8f08c7d285322824ed974
mindwave <mive.inc@gmail.com>
2019-12-20 13:32:09 +0200

    Processing the main test page

--- TelegramBotAsp/TelegramBotAsp/Controllers/HomeController.cs
+++ TelegramBotAsp/TelegramBotAsp/Controllers/HomeController.cs
@@ -3,28 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TelegramBotAsp.Helpers;
 
 namespace TelegramBotAsp.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public string Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return $"This is Telegram bot {BotSettings.BotName} main page.";
         }
     }
 }
